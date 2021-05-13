b0025ce169506c1c34460a852535bbbcba38c64b
SHREYANSHSINGH14 <shreyanshsinghchouhan1@gmail.com>
2021-05-13 13:43:37 +0530

    Updated submission page

--- backend blog-site/blogapp/modules/blog.js
+++ backend blog-site/blogapp/modules/blog.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/blog",{
+mongoose.connect("mongodb://localhost:27017/Blog",{
     useNewUrlParser:true,
     useCreateIndex:true,
+    useUnifiedTopology: true
 });
 
 var conn=mongoose.connection;
