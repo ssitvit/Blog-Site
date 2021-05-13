0c9a2328696dbc9326fba7bef8609c780e88642f
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-13 18:07:14 +0545

    single blog page renewed

--- backend blog-site/blogapp/modules/blog.js
+++ backend blog-site/blogapp/modules/blog.js
@@ -1,35 +1,39 @@
-const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Blog",{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology: true
-});
+const mongoose = require("mongoose");
+mongoose
+    .connect("mongodb://localhost:27017/Blog", {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database Connected!");
+    })
+    .catch((e) => {
+        console.log("Database COnnection Failed!!");
+    });
 
-var conn=mongoose.connection;
+var conn = mongoose.connection;
 
-const userSchema=new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
-      type: String,
-      required: true,
-      index: {
-        unique: true,
-      },
+        type: String,
+        required: true,
+        index: {
+            unique: true,
+        },
     },
     email: {
-      type: String,
-      required: true,
-      index: {
-        unique: true,
-      },
+        type: String,
+        required: true,
+        index: {
+            unique: true,
+        },
     },
     password: {
-      type: String,
-      required: true,
-      
+        type: String,
+        required: true,
     },
-    
-    
-  });
+});
 
-  const blogmodel=mongoose.model("blog",userSchema);
-  module.exports = blogmodel;
+const blogmodel = mongoose.model("blog", userSchema);
+module.exports = blogmodel;
