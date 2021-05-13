b7b00316917f213a9ca58321587d9acf14372f22
Ganesh <ganiscool@gmail.com>
2021-05-13 22:38:56 +0530

    font icons fixed

--- backend blog-site/blogapp/modules/blog.js
+++ backend blog-site/blogapp/modules/blog.js
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
 const mongoose=require('mongoose');
 mongoose.connect("mongodb://localhost:27017/blog",{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
 });
-=======
-const mongoose = require("mongoose");
-mongoose
-    .connect("mongodb://localhost:27017/Blog", {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("Database Connected!");
-    })
-    .catch((e) => {
-        console.log("Database COnnection Failed!!");
-    });
->>>>>>> 40dca282bd6f745c8251e3f7491b63ffb7c9da19
 
 var conn = mongoose.connection;
 
@@ -42,14 +26,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-<<<<<<< HEAD
     
     
     
   });
-=======
-});
->>>>>>> 40dca282bd6f745c8251e3f7491b63ffb7c9da19
 
 const blogmodel = mongoose.model("blog", userSchema);
 module.exports = blogmodel;
