16ff33436dbf95bc0cd09ba75ac9948d9319835f
Ganesh <ganiscool@gmail.com>
2021-05-14 22:33:52 +0530

    multer image upload done

--- backend blog-site/blogapp/routes/submission.js
+++ backend blog-site/blogapp/routes/submission.js
@@ -4,6 +4,12 @@ var mongoose = require("mongoose")
 //External module link
 var blogModule = require("../modules/blog_sub")
 
+// multer module 
+var multer = require("multer");
+// defining the static path for saving the image
+var path=require('path');
+router.use(express.static(__dirname + "./public"));
+
 // checking user is login or not in a function
 function checkLoginuser(req,res,next){
     var usertoken=localStorage.getItem("usertoken")
@@ -21,19 +27,34 @@ router.get('/submission',checkLoginuser, function(req, res, next) {
     res.render('submission', { title: 'Submission' });
 });
 
-router.post('/submission',(req,res)=>{
-  var author = req.body.Blog_Author;
+// diskstorage multer function
+var Storage=multer.diskStorage({
+  destination:"./public/upload/",
+  filename:(req,file,cb)=>{
+    cb(null,file.fieldname+Date.now()+path.extname(file.originalname))
+  }
+});
+
+
+// middle ware for multer 
+var upload=multer({storage:Storage}).single('file');
+
+
+router.post('/submission',upload,(req,res)=>{
+  var usertoken=localStorage.getItem("usertoken")
+  var author =usertoken ;
   var title = req.body.Blog_title;
   var desc = req.body.Blog_desc;
   var content = req.body.Blog_content;
   var usertoken=localStorage.getItem("loginuser");
-  // var image = req.body.Blog_images;
+  var image = req.file.filename;
 
   var Blog = new blogModule({
     author: author,
     title: title,
     desc: desc,
-    body: content
+    body: content,
+    image:image,
   });
   Blog.save((err, doc)=>{
     if (err) throw err;
